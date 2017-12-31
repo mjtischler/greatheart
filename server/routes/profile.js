8df2ec2faba64cbcfc003597f9680029c1ddf2a7
Matt Tischler <matt@mylawsmith.com>
2017-12-31 14:35:13 -0500

    Adding api email and username searches; basic error handling on client and server sides; proper login/signup redirecting

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -2,17 +2,17 @@
 
 const express = require('express');
 const router = express.Router();
-const db = require('../db/db-access');
+// const db = require('../db/db-access');
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-  db.searchCollection('test')
-    .then(resolve => {
-      res.json([resolve.result]);
-    })
-    .catch(reject => {
-      console.log(reject.status, reject.result);
-    });
+router.get('/profile', (req, res) => {
+  // db.searchCollection('test')
+  //   .then(resolve => {
+  //     res.json([resolve.result]);
+  //   })
+  //   .catch(reject => {
+  //     console.log(reject.status, reject.result);
+  //   });
 });
 
 module.exports = router;
